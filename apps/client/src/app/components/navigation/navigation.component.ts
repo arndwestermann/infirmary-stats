b19ba7b1fdfa5745695bd981b0cedcee0ed3633a
Arnd Westermann <arnd.westermann@precess.de>
2024-11-01 14:37:57 +0100

    feat(navigation): added animation

--- apps/client/src/app/components/navigation/navigation.component.ts
+++ apps/client/src/app/components/navigation/navigation.component.ts
@@ -4,6 +4,7 @@ import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/rou
 import { TuiButton, TuiIcon } from '@taiga-ui/core';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { NgClass } from '@angular/common';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 const angularImports = [RouterOutlet, RouterLink, RouterLinkActive, NgClass];
 const taigaUiImports = [TuiButton, TuiIcon];
@@ -15,11 +16,43 @@ const thirdPartyImports = [TranslocoDirective];
 	imports: [DrawerContainerComponent, DrawerComponent, ...angularImports, ...taigaUiImports, ...thirdPartyImports],
 	templateUrl: './navigation.component.html',
 	styles: `
-    :host {
-      @apply flex flex-col h-full;
-    }
-  `,
+		:host {
+			@apply flex flex-col h-full;
+		}
+	`,
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	animations: [
+		trigger('nav-animation-icon', [
+			state(
+				'opened',
+				style({
+					transform: 'translateX(0)',
+				}),
+			),
+			state(
+				'closed',
+				style({
+					transform: 'translateX(12rem)',
+				}),
+			),
+			transition('* <=> *', animate('300ms cubic-bezier(0, 0, 0.3, 1)')),
+		]),
+		trigger('nav-animation-text', [
+			state(
+				'opened',
+				style({
+					opacity: 1,
+				}),
+			),
+			state(
+				'closed',
+				style({
+					opacity: 0,
+				}),
+			),
+			transition('* <=> *', animate('10ms cubic-bezier(0, 0, 0.3, 1)')),
+		]),
+	],
 })
 export class NavigationComponent {
 	private readonly router = inject(Router);
